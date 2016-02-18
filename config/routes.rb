cead6bafb1416cb06974fcf370e10d8cf6a45190
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-19 10:34:02 +1300

    Remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :cards, only: [:index, :show, :new, :create, :destroy]
+  resources :cards, only: [:index, :show, :create, :destroy]
 
-  resources :deck_lists, only: [:index, :show, :new, :create, :destroy] do
+  resources :deck_lists, only: [:index, :show, :create, :destroy] do
     resources :card_requirements, only: [:create, :destroy, :update]
   end
 end
