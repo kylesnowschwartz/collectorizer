e1957fbd54b0cf00543177667fa2621e7e3f9bc0
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-19 21:15:26 +1300

    Update the update card requirements is deck service to conditionally update or delete a card requirement based on the new amount of cards required

--- app/services/update_card_requirement_in_deck_list.rb
+++ app/services/update_card_requirement_in_deck_list.rb
@@ -5,6 +5,28 @@ class UpdateCardRequirementInDeckList
   end
 
   def call
+    CardRequirement.transaction do
+      if @quantity_required > 0
+        update_card_requirement
+      elsif @quantity_required <= 0
+        remove_card_requirement
+      end
+
+      deck_list.reload
+    end
+  end
+
+  private
+
+  def deck_list
+    DeckList.find(@card_requirement.deck_list_id)
+  end
+
+  def update_card_requirement
     @card_requirement.update_attributes!(quantity_required: @quantity_required)
+  end
+
+  def remove_card_requirement
+    RemoveCardRequirementFromDeckList.new(@card_requirement).call
   end
 end
