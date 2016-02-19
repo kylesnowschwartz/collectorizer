47126be442cc79061fc5008e9ea2e1669f035d14
Matt Powell <matt.powell@powershop.co.nz>
2016-02-19 15:41:01 +1300

    text box to update card quantities numerically

--- app/assets/javascripts/models/collection.coffee
+++ app/assets/javascripts/models/collection.coffee
@@ -6,3 +6,20 @@ class App.Models.Collection extends Array
   quantity: (multiverse) ->
     return card.quantity for card in this when card.multiverse == multiverse
     0
+
+  setQuantity: (card, quantity) ->
+    if quantity > 0
+      for existing in this when existing.multiverse == card.multiverse
+        existing.quantity = quantity
+        return
+      @push($.extend({}, card, { quantity }))
+    else
+      @remove(card.multiverse)
+
+  remove: (multiverse) ->
+    i = 0
+    while i < @length
+      if this[i].multiverse == multiverse
+        @splice(i, 1)
+      else
+        i++
--- app/services/remove_card_from_collection.rb
+++ app/services/remove_card_from_collection.rb
@@ -1,21 +1,21 @@
 class RemoveCardFromCollection
   def initialize(card, user)
     @card = card
-    @card_name = card.name
+    @multiverse = card.multiverse
     @user = user
   end
 
   def call
     @card.destroy!
-    
+
     update_decklists
   end
 
   private
 
   def update_decklists
-    @user.decklists.each do |decklist|
-      decklist.card_requirements.cards_with_name(@card_name).each do |card_requirement|
+    @user.deck_lists.each do |decklist|
+      decklist.card_requirements.cards_with_multiverse(@multiverse).each do |card_requirement|
         quantity_owned = card_requirement.quantity_owned
 
         card_requirement.update_attributes!(quantity_owned: quantity_owned - 1)
