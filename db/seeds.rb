0215c7d2e905a9b7963607aaa5662c2503414568
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2016-02-19 15:55:04 +1300

    fixed seeds file uncommented things

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,43 @@
 require 'csv'
 
 # You'll need to create a user before running this seedfile
-# user = User.create!(email: "kyle.snowschwartz@gmail.com", password: "password")
+user = User.create!(email: "kyle.snowschwartz@gmail.com", password: "password")
 
-# CSV.foreach("./db/have_list.csv") do |row|
-#   p row[1]
+CSV.foreach("./db/have_list.csv") do |row|
+  AddCardToCollection.new(row[1], user).call
+end
 
-#   AddCardToCollection.new(row[1], user).call
-# end
+deck_list = CreateDeckList.new("Brago1", user).call
 
-# deck_list = CreateDeckList.new("Brago1", user).call
+File.foreach("./db/brago_deck_list_1.txt") do |line_item|
+  unless line_item == "\n"
+    line_item = line_item.chomp
 
-# File.foreach("./db/brago_deck_list_1.txt") do |line_item|
-#   unless line_item == "\n"
-#     line_item = line_item.chomp
+    quantity_required = line_item.split(" ")[0].to_i
 
-#     quantity_required = line_item.split(" ")[0].to_i
+    card_name = line_item.split(" ")[1..-1].join(" ")
 
-#     card_name = line_item.split(" ")[1..-1].join(" ")
+    AddCardRequirementToDeckList.new(deck_list, card_name, quantity_required).call
+  end
+end
 
-#     AddCardRequirementToDeckList.new(deck_list, card_name, quantity_required).call
-#   end
-# end
+deck_list_two = CreateDeckList.new("Zedruu1", user).call
 
-# deck_list_two = CreateDeckList.new("Zedruu1", user).call
+File.foreach("./db/zedruu_sample.txt") do |line_item|
+  unless line_item == "\n"
+    line_item = line_item.chomp
 
-# File.foreach("./db/zedruu_sample.txt") do |line_item|
-#   unless line_item == "\n"
-#     line_item = line_item.chomp
+    quantity_required = line_item.split(" ")[0].to_i
 
-#     quantity_required = line_item.split(" ")[0].to_i
+    card_name = line_item.split(" ")[1..-1].join(" ")
 
-#     card_name = line_item.split(" ")[1..-1].join(" ")
+    AddCardRequirementToDeckList.new(deck_list_two, card_name, quantity_required).call
+  end
+end
 
-#     AddCardRequirementToDeckList.new(deck_list_two, card_name, quantity_required).call
-#   end
-# end
-
-# ["Plains", "Island", "Mountain", "Swamp", "Forest"].each do |basic_land|
-#   10.times { AddCardToCollection.new(basic_land, user).call }
-# end
+["Plains", "Island", "Mountain", "Swamp", "Forest"].each do |basic_land|
+  10.times { AddCardToCollection.new(basic_land, user).call }
+end
 
 gods_deck_list = CreateDeckList.new("Gods1", user).call
 
